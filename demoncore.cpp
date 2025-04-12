04aa4dcd626e2a1042e49ecb4685e36697d08c2e
6ei6ei6 <6ei6ei6@gmail.com>
2025-04-12 14:04:38 -0500

    Refactor entity storage and rework item spawn

--- demoncore.cpp
+++ demoncore.cpp
@@ -8,7 +8,7 @@
 #include <algorithm>
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/Font.hpp>
-int;
+
 constexpr int WINDOW_WIDTH = 1000;
 constexpr int WINDOW_HEIGHT = 1000;
 
@@ -43,7 +43,7 @@ constexpr float ENEMY_SPREAD = 200;
 constexpr float ENEMY_BOUNCE = 30;
 constexpr float ENEMY_LINE = 1000;
 
-const float FLOCK_RADIUS = 2000;
+const float FLOCK_RADIUS = 1500;
 const float FLOCK_SPEED = 20;
 const float FLOCK_SEPARATION = 10.0;
 const float FLOCK_ALIGNMENT = 10.0;
@@ -72,12 +72,14 @@ constexpr float HUD_SIZE = 40;
 constexpr size_t ITEM_MAX = 100;
 constexpr size_t ENEMY_MAX = 2000;
 constexpr size_t BULLET_MAX = 500;
+constexpr size_t ENTITY_MAX = ITEM_MAX + ENEMY_MAX + BULLET_MAX;
 
 // GAME STATES
 constexpr uint8_t GAME_PAUSE = 1 << 0;
 constexpr uint8_t GAME_DEBUG = 1 << 1;
 constexpr uint8_t GAME_VFX = 1 << 2;
 constexpr uint8_t GAME_HUD = 1 << 3;
+constexpr uint8_t GAME_STATS = 1 << 4;
 // ....
 
 // INPUT FLAGS
@@ -98,6 +100,10 @@ constexpr uint8_t STATE_MOVING = 1 << 1;
 constexpr uint8_t STATE_GRABBED = 1 << 2;
 constexpr uint8_t STATE_HURT = 1 << 3;
 // spares......
+
+// ALIVE - Rendering
+// SLEEP - No physics
+// 
 
 // ENTITY TYPES
 constexpr uint8_t ENTITY_ITEM = 1 << 0;
@@ -127,31 +133,29 @@ sf::Vector2f dragStart = { 0,0 };
 sf::Vector2f position = { WORLD_WIDTH * 0.5 , WORLD_HEIGHT * 0.5 };
 sf::Vector2f velocity = { 0,0 };
 
+sf::Vector2f mousePos;
+
 sf::FloatRect deadZone;
 
-sf::Font font;
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
-sf::Event event;
+sf::Vector2f positions[ENTITY_MAX];
+sf::Vector2f velocities[ENTITY_MAX];
 
-sf::CircleShape player(ENTITY_RADIUS, 3);
-sf::CircleShape item(ENTITY_RADIUS, 4);
-sf::CircleShape enemy(ENTITY_RADIUS, 3);
-sf::CircleShape bullet(ENTITY_RADIUS, 3);
-sf::CircleShape vfx(ENTITY_RADIUS, 3);
+float rotations[ENTITY_MAX];
 
-sf::Text HUD("HUD", font, HUD_SIZE);
+uint8_t states[ENTITY_MAX];
 
-sf::RenderWindow window(sf::VideoMode({ WINDOW_WIDTH, WINDOW_HEIGHT }), "demoncore");
-sf::View view({ 0.f,200.f }, { WINDOW_WIDTH, WINDOW_HEIGHT });
-const sf::View pauseView = window.getDefaultView();
-const sf::View hudView = window.getDefaultView();
+unsigned int itemIDs[ITEM_MAX];
+unsigned int enemyIDs[ENEMY_MAX];
+unsigned int bulletIDs[BULLET_MAX];
 
-struct ItemPool
-{
-	uint8_t state[ITEM_MAX];
-	sf::Vector2f position[ITEM_MAX];
-	sf::Vector2f velocity[ITEM_MAX];
-} items;
+unsigned int entityCount = 0;
+unsigned int itemCount = 0;
+// unsigned int enemyCount = 0;
+// unsigned int bulletCount = 0;
+
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 struct EnemyPool
 {
@@ -169,6 +173,65 @@ struct BulletPool
 	float rotation[BULLET_MAX];
 } bullets;
 
+sf::Font font;
+sf::Event event;
+sf::Text HUD("HUD", font, HUD_SIZE);
+
+sf::CircleShape player(ENTITY_RADIUS, 3);
+sf::CircleShape item(ENTITY_RADIUS, 4);
+sf::CircleShape enemy(ENTITY_RADIUS, 3);
+sf::CircleShape bullet(ENTITY_RADIUS, 3);
+sf::CircleShape vfx(ENTITY_RADIUS, 3);
+
+sf::RenderWindow window(sf::VideoMode({ WINDOW_WIDTH, WINDOW_HEIGHT }), "demoncore");
+sf::View view({ 0.f,200.f }, { WINDOW_WIDTH, WINDOW_HEIGHT });
+const sf::View pauseView = window.getDefaultView();
+const sf::View hudView = window.getDefaultView();
+
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+// HELPERS //
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+// All-encompasing spawn entity function.
+int spawnEntity(sf::Vector2f pos, sf::Vector2f vel, float rot, uint8_t state)
+{
+	if (entityCount == ENTITY_MAX) return -1;
+
+	int id = entityCount++;
+
+	positions[id] = pos;
+	velocities[id] = vel;
+	rotations[id] = rot;
+	states[id] = state;
+
+	return id;
+}
+
+void spawnItem(sf::Vector2f pos)
+{
+	if (itemCount >= ITEM_MAX) return;
+
+	int id = spawnEntity(pos, {}, 0, STATE_ALIVE);
+
+	if (id != -1) itemIDs[itemCount++] = id;
+}
+
+void spawnEnemy(sf::Vector2f pos)
+{
+	if (enemyCount == ENEMY_MAX) return;
+
+	int id = spawnEntity(pos, {}, 0, STATE_ALIVE);
+	if (id != -1) enemyIDs[enemyCount++] = id;
+}
+
+void spawnBullet(sf::Vector2f pos, sf::Vector2f vel, float rot)
+{
+	if (bulletCount == BULLET_MAX) return;
+
+	int id = spawnEntity(pos, vel, rot, STATE_ALIVE);
+	if (id != -1) bulletIDs[bulletCount++] = id;
+}
+
+
 int main()
 {
 	if (!font.loadFromFile("font.ttf"))
@@ -179,17 +242,10 @@ int main()
 
 	for (int i = 0; i < ENEMY_MAX; i++)
 	{
-		enemies.state[i] = 0b1;
+		//enemies.state[i] = 0b1;
 		enemies.health[i] = 100;
 		enemies.position[i] = { static_cast<float>(rand() % WORLD_WIDTH), static_cast<float>(rand() % WORLD_HEIGHT) };
 		enemies.velocity[i] = { 0,0 };
-	}
-
-	for (int i = 0; i < ITEM_MAX; i++)
-	{
-		items.state[i] = 0b1;
-		items.position[i] = { static_cast<float>(rand() % WORLD_WIDTH), static_cast<float>(rand() % WORLD_HEIGHT) };
-		items.velocity[i] = { 0,0 };
 	}
 
 	for (int i = 0; i < BULLET_MAX; i++)
@@ -204,6 +260,25 @@ int main()
 		if (enemies.state[i] & STATE_ALIVE) enemyCount++;
 		else enemyCount--;
 	}
+
+
+
+
+
+
+	// SPAWN ITEMS
+	for (unsigned int i = 0; i < ITEM_MAX; i++)
+	{
+		if (itemCount >= ITEM_MAX) break;
+
+		sf::Vector2f spawnPos = {	static_cast<float>(rand() % WORLD_WIDTH),
+									static_cast<float>(rand() % WORLD_HEIGHT)};
+
+		spawnItem(spawnPos);
+	}
+
+
+
 
 	deadZone.width = DEADZONE_WIDTH;
 	deadZone.height = DEADZONE_HEIGHT;
@@ -241,24 +316,30 @@ int main()
 	view.setCenter(player.getPosition());
 	window.setFramerateLimit(FPS_MAX);
 	
-	// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-	// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	// [][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][]
+	// WINDOW LOOP //
+	// [][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][]
 	while (window.isOpen())
 	{
 		float deltaTime = deltaClock.restart().asSeconds();
 		if (playerState & 0b1) window.clear();
 
+		// UPDATE MOUSE POSITION GLOBAL
+		mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+
 		// UPDATE AIM GLOBAL
 		// Expensive calculation. Use sparingly and make the most of it.
-		sf::Vector2f dirAim = window.mapPixelToCoords(sf::Mouse::getPosition(window)) - player.getPosition();
+		sf::Vector2f dirAim = mousePos - player.getPosition();
 		float distSqAim = dirAim.x * dirAim.x + dirAim.y * dirAim.y;
 		float distAim = std::sqrt(distSqAim);
 		if (distAim == 0.0f) distAim = 0.001f;
 		sf::Vector2f normalAim = dirAim / distAim;
 		aim = normalAim;
 
-		///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+		// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 		// PLAYER INPUT & EVENTS //
+		// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 		// INPUTS
 		while (window.pollEvent(event))
 		{
@@ -268,7 +349,13 @@ int main()
 
 			if (event.type == sf::Event::KeyPressed) if (event.key.code == sf::Keyboard::End) playerState ^= 0b1;
 
-			if (gameState & GAME_PAUSE || !(playerState & STATE_ALIVE)) break;
+			if (gameState & GAME_PAUSE)
+			{
+				if (event.type == sf::Event::MouseButtonPressed) if (event.mouseButton.button == sf::Mouse::Right) gameState ^= GAME_STATS;
+			}
+			else gameState &= ~GAME_STATS;
+
+			if (!(playerState & STATE_ALIVE)) break;
 
 			if (event.type == sf::Event::KeyPressed)
 			{
@@ -349,35 +436,42 @@ int main()
 		{
 			if (grabbed == -1)
 			{
-				for (size_t i = 0; i < ITEM_MAX; i++)
+				for (unsigned int i = 0; i < itemCount; i++)
 				{
-					sf::Vector2f dirMouse = window.mapPixelToCoords(sf::Mouse::getPosition(window)) - items.position[i];
-					float distSqMouse = dirMouse.x * dirMouse.x + dirMouse.y * dirMouse.y;
+					unsigned int id = itemIDs[i];
 
-					if (distSqMouse < GRAB_DISTANCE)
+					if (states[i] & STATE_ALIVE)
 					{
-						grabbed = i;
-						items.state[i] |= STATE_GRABBED;
+						sf::Vector2f dist = mousePos - positions[id];
+						float distSq = dist.x * dist.x + dist.y + dist.y;
+
+						if (distSq < GRAB_DISTANCE)
+						{
+							grabbed = i;
+							states[id] |= STATE_GRABBED;
+						}
 					}
 				}
+
 			}
 		}
 
+		
 		// HANDLE DRAG INPUT
 		if (playerInput & INPUT_DRAG)
 		{
-			if (playerInput & INPUT_GRAB && grabbed > -1)
-			{
-				items.position[grabbed] = static_cast<sf::Vector2f>(window.mapPixelToCoords(sf::Mouse::getPosition(window)));
-				dragStart = items.position[grabbed];
+			int id = itemIDs[grabbed];
 
-				dragTimer.restart();
+			if (playerInput & INPUT_DRAG && grabbed > -1)
+			{
+				positions[id] = mousePos;
+				dragStart = positions[id];
 			}
 			else if (grabbed > -1)
 			{
 				if (dragTimer.getElapsedTime().asSeconds() >= DRAG_TIME)
 				{
-					
+					std::cout << "time\n";
 					sf::Vector2f dirDrag = static_cast<sf::Vector2f>(window.mapPixelToCoords(sf::Mouse::getPosition(window))) - dragStart;
 					float distSqDrag = dirDrag.x * dirDrag.x + dirDrag.y * dirDrag.y;
 
@@ -387,8 +481,8 @@ int main()
 						if (distDrag == 0.0f) distDrag = 0.0001f;
 						sf::Vector2f normalDrag = static_cast<sf::Vector2f>(dirDrag) / distDrag;
 						
-						items.state[grabbed] &= ~STATE_GRABBED;
-						items.velocity[grabbed] = normalDrag * DROP_FORCE;
+						states[id] &= ~STATE_GRABBED;
+						velocities[id] = normalDrag * DROP_FORCE;
 					}
 
 					if (grabbed > -1) grabbed = -1;
@@ -403,8 +497,9 @@ int main()
 		{
 			if (grabbed > -1)
 			{
+				std::cout << "drop\n";
 				dragTimer.restart();
-				// grabbed = -1;
+				//grabbed = -1;
 			}
 			playerInput &= ~INPUT_DROP;
 		}
@@ -446,21 +541,31 @@ int main()
 		if (view.getSize().x < 1 || view.getSize().y < 1) view.setSize({ 1,1 });
 		if (view.getSize().x > WORLD_WIDTH || view.getSize().y > WORLD_HEIGHT) view.setSize({ WORLD_WIDTH,WORLD_HEIGHT });
 
-		///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		
+
+
+
+
+
+
+
+
+
+		// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 		// UPDATE STATES //
+		// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 		// UPDATE HEALTH
-		if (health == 0)
-		{
-			playerState &= ~0b1;
-			playerInput &= ~0b11111111;
-		}
+		if (health == 0) playerState &= ~0b1;
 
 		if (health < PLAYER_HEALTH && regenTimer.getElapsedTime().asSeconds() > REGEN_TIME) health += REGEN_SPEED * deltaTime;
 
-		///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		// ############################################################################################################################
 		// COLLISIONS AND INTERACTIONS //
+		// ############################################################################################################################
 		if ((gameState & GAME_PAUSE) == 0 && playerState & STATE_ALIVE)
 		{
+			/*
 			// ITEM COLLISION
 			for (size_t i = 0; i < ITEM_MAX; i++)
 			{
@@ -487,6 +592,7 @@ int main()
 					}
 				}
 			}
+			*/
 
 			// BULLET COLLISION
 			for (size_t i = 0; i < BULLET_MAX; i++)
@@ -515,7 +621,7 @@ int main()
 						}
 					}
 				}
-
+				/*
 				// BULLET-ITEM COLLISION
 				for (size_t k = 0; k < ITEM_MAX; k++)
 				{
@@ -530,7 +636,7 @@ int main()
 						bullets.state[i] &= ~0b1;
 					}
 				}
-
+				*/
 				// BULLET-WINDOW FRAME COLLISION
 				sf::FloatRect viewBounds;
 				sf::View currentView = window.getView();
@@ -557,14 +663,6 @@ int main()
 					// playerState &= ~0b1; // INVINSIBILITY &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 				}
 
-				// ENEMY-ITEM COLLISION
-				for (size_t k = 0; k < ITEM_MAX; k++)
-				{
-					if (items.state[i] & STATE_ALIVE)
-					{
-
-					}
-				}
 
 				// ENEMY FLOCK & HORDE BEHAVIOR $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 				sf::Vector2f pos = enemies.position[i];
@@ -633,11 +731,31 @@ int main()
 			}
 		}
 
-		///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 		// UPDATE PHYSICS //
+		// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 		// Mostly update arrays and state.
 		if ((gameState & GAME_PAUSE) == 0)
 		{
+			/*
 			// UPDATE ITEM PHYSICS
 			// Adds velocity to the position.
 			for (size_t i = 0; i < ITEM_MAX; i++)
@@ -647,6 +765,18 @@ int main()
 				items.position[i] += items.velocity[i] * deltaTime;
 
 				items.velocity[i] *= ITEM_FRICTION;
+			}
+			*/
+
+			for (unsigned int i = 0; i < itemCount; i++)
+			{
+				unsigned int id = itemIDs[i];
+
+				if (!(states[id] & STATE_ALIVE)) continue;
+
+				positions[id] += velocities[id] * deltaTime;
+
+				velocities[id] *= ITEM_FRICTION;
 			}
 
 			// UPDATE ENEMY PHYSICS
@@ -691,8 +821,49 @@ int main()
 			}
 		}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
+
+
+
+
+
+
+
 		///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		// SHAPE SETTING & RENDERING & VFX & DEBUG //
+		///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+		// RENDER ITEMS
+		for (unsigned int i = 0; i < itemCount; i++)
+		{
+			unsigned int id = itemIDs[i];
+
+			if (states[id] & STATE_ALIVE)
+			{
+				item.setPosition(positions[id]);
+				window.draw(item);
+			}
+		}
+
+
+
+
 		// ENEMY VFX
 		for (size_t i = 0; i < ENEMY_MAX; i++)
 		{
@@ -729,6 +900,7 @@ int main()
 			//sf::Vertex aimLine[] = { sf::Vertex(player.getPosition(), vfxColor), sf::Vertex(player.getPosition() + aim * 50.f, sf::Color::Transparent) };
 			//window.draw(aimLine, 2, sf::Lines);
 
+			/*
 			// Draw entity lines
 			if (grabbed > -1)
 			{
@@ -746,6 +918,7 @@ int main()
 				
 				window.draw(vfx);
 			}
+			*/
 		}
 		else
 		{
@@ -764,15 +937,6 @@ int main()
 				bullet.setRotation(bullets.rotation[i]);
 				bullet.setPosition(bullets.position[i]);
 				window.draw(bullet);
-			}
-		}
-
-		for (size_t i = 0; i < ITEM_MAX; i++)
-		{
-			if (items.state[i] & STATE_ALIVE)
-			{
-				item.setPosition(items.position[i]);
-				window.draw(item);
 			}
 		}
 
@@ -856,9 +1020,6 @@ int main()
 		}
 		
 		// HUD & USER INTERFACE *******************************************************************************************************
-		// HUD & USER INTERFACE *******************************************************************************************************
-		// HUD & USER INTERFACE *******************************************************************************************************
-		// HUD & USER INTERFACE *******************************************************************************************************
 		// PLAYER HEALTH
 		if (gameState & GAME_HUD)
 		{
@@ -877,9 +1038,6 @@ int main()
 			window.draw(HUD);
 		}
 
-		// GAME OVER SCREEN ***********************************************************************************************************
-		// GAME OVER SCREEN ***********************************************************************************************************
-		// GAME OVER SCREEN ***********************************************************************************************************
 		// GAME OVER SCREEN ***********************************************************************************************************
 		// DRAW TEXT & CALCULATE KILL AREA
 		if ((playerState & STATE_ALIVE) == 0)
@@ -949,6 +1107,7 @@ int main()
 					}
 				}
 
+				/*
 				// DEATH-ITEM COLLISION
 				for (size_t k = 0; k < ITEM_MAX; k++)
 				{
@@ -963,12 +1122,10 @@ int main()
 						bullets.state[i] &= ~0b1;
 					}
 				}
+				*/
 			}
 		}
 
-		// PAUSE MENU SCREEN **********************************************************************************************************
-		// PAUSE MENU SCREEN **********************************************************************************************************
-		// PAUSE MENU SCREEN **********************************************************************************************************
 		// PAUSE MENU SCREEN **********************************************************************************************************
 		if (gameState & GAME_PAUSE)
 		{
@@ -1031,6 +1188,33 @@ int main()
 			window.draw(countText);
 		}
 		
+		// GAME STATS SCREEN **********************************************************************************************************
+		if (gameState & GAME_STATS)
+		{
+			window.clear(sf::Color::White);
+
+			sf::Text pauseText("The game is paused. Press [Esc] to continue.", font, 24);
+			//sf::Text enemyCountText("enemy count: ", font, 24);
+			//sf::Text itemCountText("item count: ", font, 24);
+
+			pauseText.setFillColor(sf::Color::Black);
+			pauseText.setPosition(WINDOW_WIDTH * 0.5, WINDOW_HEIGHT * 0.2);
+			pauseText.setOrigin(pauseText.getLocalBounds().width * 0.5, pauseText.getLocalBounds().height * 0.5);
+			
+			//enemyCountText.setFillColor(sf::Color::Black);
+			//enemyCountText.setPosition(WINDOW_WIDTH * 0.3, WINDOW_HEIGHT * 0.45);
+			//enemyCountText.setOrigin(enemyCountText.getLocalBounds().width * 0.5, enemyCountText.getLocalBounds().height * 0.5);
+
+			//itemCountText.setFillColor(sf::Color::Black);
+			//itemCountText.setPosition(WINDOW_WIDTH * 0.3, WINDOW_HEIGHT * 0.55);
+			//itemCountText.setOrigin(itemCountText.getLocalBounds().width * 0.5, itemCountText.getLocalBounds().height * 0.5);
+			
+			window.draw(pauseText);
+			//window.draw(enemyCountText);
+			//window.draw(itemCountText);
+		}
+
+		// ****************************************************************************************************************************
 		window.setView(view);
         window.display();
     }
